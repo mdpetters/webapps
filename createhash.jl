fc928a3f4c7729afecf1600aaee667e9db002306
Markus Petters <mdpetter@ncsu.edu>
2022-01-28 09:26:42 +0100

    add hash function

---
+++ createhash.jl
@@ -0,0 +1,11 @@
+# Create a hash of the log for an untainted webapp container
+# It is important that the log is not stateful, i.e. that the 
+# log output is always the same after the container is fully initialized. 
+
+using SHA
+
+file = "swarm_virtualdma"    # hashfile
+ID = "9cbcd28d607f"          # ID of an untainted fully executed app
+hashlog(ID) = (read(`docker logs $(ID)`) |> sha256 |> bytes2hex)
+
+open(f -> write(f, hashlog(ID)), file, "w")
